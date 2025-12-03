f5b032afb7a94abc038d04f5579569b7344b99b5
John Min <john.min@sap.com>
2025-12-03 17:11:26 +0800

    2025 day 3 part 2

--- 2025/day03/day3.py
+++ 2025/day03/day3.py
@@ -1,4 +1,3 @@
-from itertools import pairwise
 import sys
 
 
@@ -6,16 +5,20 @@ fp = sys.argv[1]
 with open(fp, "r") as file:
     lines = file.read().split()
 
-def max_joltage(bank: str) -> int:
-    max1, max2 = -1, -1
-    for bat1, bat2 in pairwise(bank):
-        bat1, bat2 = int(bat1), int(bat2)
-        if max1 == -1 or max1 < bat1:
-            max1 = bat1
-            max2 = bat2
-        elif max2 < bat2:
-            max2 = bat2
-    return max1 * 10 + max2
+def max_joltage(bank: str, n=12) -> int:
+    m = len(bank)
+    left = 0
+    res = 0
+    for right in range(m - n, m):
+        max_idx, max_bat = -1, -1
+        for j in range(left, right + 1):
+            bat = int(bank[j])
+            if max_bat < bat:
+                max_idx = j
+                max_bat = bat
+        res = res * 10 + max_bat
+        left = max_idx + 1
+    return res
 
 ans = 0
 for bank in lines:
