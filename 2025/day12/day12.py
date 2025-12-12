6d5920230607d1f0439b91061e538a1bde6f0e36
John Min <minjohn0227@gmail.com>
2025-12-13 00:52:57 +0800

    2025 day 12 part 1 (heuristic based)
    does not work for sample test input
    
    heuristic taken from other examples I found online

--- 2025/day12/day12.py
+++ 2025/day12/day12.py
@@ -18,7 +18,6 @@ def read_content(lines: list[str]):
         shapes[-1].append(list(lines.pop()))
         # discard empty line
         lines.pop()
-    shapes.sort(key=lambda shape: sum(1 for row in shape for val in row if val == "."))
 
     # list of ((n, m), (..quantity of each shape))
     regions = []
@@ -30,79 +29,18 @@ def read_content(lines: list[str]):
 
     return shapes, regions
 
-def get_rotations(shapes: list[list[list[str]]]):
-    rotations = []
-    for shape in shapes:
-        rotations.append([])
-        rot = [
-            [[int(shape[r][c] == "#") << (2 - c) for c in range(3)] for r in range(3)],
-            [[int(shape[r][c] == "#") << r for r in range(2, -1, -1)] for c in range(3)],
-            [[int(shape[r][c] == "#") << c for c in range(2, -1, -1)] for r in range(2, -1, -1)],
-            [[int(shape[r][c] == "#") << (2 - r) for r in range(3)] for c in range(2, -1, -1)],
-        ]
+def can_fill(region, shape_areas):
+    (cols, rows), shape_counts = region
+    needed_area = sum(shape_areas[i] * count for i, count in enumerate(shape_counts))
 
-        seen = set()
-        for r in rot:
-            rkey = tuple(sum(row) for row in r)
-            if rkey not in seen:
-                rotations[-1].append(list(rkey))
-                seen.add(rkey)
-    return rotations
+    # assume we need 20% more than we actually need
+    return (cols * rows) >= int(1.2 * needed_area)
 
-def can_fill_region(region, rotations) -> bool:
-    def empty_points(grid):
-        # subtract 2 to only get empty points that we can place shapes on
-        #   -> shapes are all 3x3
-        return [(r, c) for r in range(rows - 2)
-                       for c in range(cols - 2)
-                       if grid[r] & (1 << (cols - c - 1)) == 0]
-
-    def can_place_shape(r, c, grid, shape):
-        return grid[r]     & (shape[0] << (cols - c - 3)) == 0 and \
-               grid[r + 1] & (shape[1] << (cols - c - 3)) == 0 and \
-               grid[r + 2] & (shape[2] << (cols - c - 3)) == 0
-
-    def fill_shape(r, c, grid, shape):
-        grid[r]     |= shape[0] << (cols - c - 3)
-        grid[r + 1] |= shape[1] << (cols - c - 3)
-        grid[r + 2] |= shape[2] << (cols - c - 3)
-
-    def remove_shape(r, c, grid, shape):
-        grid[r]     ^= shape[0] << (cols - c - 3)
-        grid[r + 1] ^= shape[1] << (cols - c - 3)
-        grid[r + 2] ^= shape[2] << (cols - c - 3)
-
-    def backtrack(i, scheme, grid, rotations, seen):
-        if i == len(scheme):
-            return True
-
-        rot_idx = scheme[i]
-        for r, c in empty_points(grid):
-            for rotation in rotations[rot_idx]:
-                if can_place_shape(r, c, grid, rotation):
-                    fill_shape(r, c, grid, rotation)
-                    seenkey = tuple(grid), i
-                    if not seenkey in seen:
-                        seen.add(seenkey)
-                        if backtrack(i + 1, scheme, grid, rotations, seen):
-                            return True
-                    remove_shape(r, c, grid, rotation)
-        return False
-
-    (cols, rows), shapes = region
-    grid = [0 for _ in range(rows)]
-
-    scheme = []
-    for i, num in enumerate(shapes):
-        scheme.extend([i] * num)
-    return backtrack(0, scheme, grid, rotations, set())
 
 shapes, regions = read_content(lines)
-rotations = get_rotations(shapes)
+areas = [sum(1 for row in shape for val in row if val == "#") for shape in shapes]
 
 ans = 0
 for region in regions:
-    can_fill = can_fill_region(region, rotations)
-    if can_fill:
-        ans += 1
+    ans += int(can_fill(region, areas))
 print(ans)
